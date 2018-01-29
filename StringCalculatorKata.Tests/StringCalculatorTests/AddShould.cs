e41e41c9e4d9a8b5f5622fa2201b82e07c638f6a
Andre Guedes <andre.guedes@asos.com>
2018-01-29 16:48:23 +0000

    First commit

---
+++ StringCalculatorKata.Tests/StringCalculatorTests/AddShould.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringCalculatorKata.Tests.StringCalculatorTests
+{
+    [TestFixture]
+    public class AddShould
+    {
+        [Test]
+        public void ReturnZero_WhenAddReceivesEmptyString()
+        {
+            var expectedResult = 0;
+
+            var actual = StringCalculator.Add(string.Empty);
+
+            actual.Should().Be(expectedResult);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        [TestCase("1000000000", 1000000000)]
+        public void ReturnExpectedResult_WhenAddReceivesStringWithOneNumber(string oneNumber, int expectedResult)
+        {
+            var actual = StringCalculator.Add(oneNumber);
+
+            actual.Should().Be(expectedResult);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("5, 10", 15)]
+        [TestCase("2,3", 5)]
+        public void ReturnExpectedResult_WhenAddReceivesStringWithTwoNumbers(string twoNumbers, int expectedResult)
+        {
+            var actual = StringCalculator.Add(twoNumbers);
+
+            actual.Should().Be(expectedResult);
+        }
+
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
+        [TestCase("1,2,3,4,5", 15)]
+        public void ReturnExpectedResult_WhenAddReceivesUnknownAmountOfNumbers(string unknownAmountOfNumbers,
+            int expectedResult)
+        {
+            var actual = StringCalculator.Add(unknownAmountOfNumbers);
+
+            actual.Should().Be(expectedResult);
+        }
+
+        [TestCase("1\n2,3", 6)]
+        [TestCase("1,2\n3", 6)]
+        [TestCase("1\n2,3\n4,5", 15)]
+        public void ReturnExpectedResult_WhenAddReceivesNumbersWithDifferentSeparators(
+            string numbersWithDifferentSeparators, int expectedResult)
+        {
+            var actual = StringCalculator.Add(numbersWithDifferentSeparators);
+
+            actual.Should().Be(expectedResult);
+        }
+
+        [TestCase("//;\n4;2", 6)]
+        [TestCase("//.\n1.2.3", 6)]
+        [TestCase("//#\n1#2#3#4", 10)]
+        public void ReturnExpectedResult_WhenAddReceivesNumbersWithCustomSeparators(string numbersWithCustomSeparators,
+            int expectedResult)
+        {
+            var actual = StringCalculator.Add(numbersWithCustomSeparators);
+
+            actual.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            var zero = 0;
+
+            if (numbers.StartsWith("//"))
+            {
+                var pattern = @"\/\/(.)\n(([0-9]+)\1*)*";
+
+                var numbersSplit = from capture
+                        in Regex.Matches(numbers, pattern)[0].Groups[3].Captures.Cast<Capture>()
+                    select capture.Value;
+
+                return numbersSplit.Sum(Convert.ToInt32);
+            }
+
+            if (numbers.Equals(string.Empty))
+                return zero;
+
+            var comaSeparator = ',';
+            var newLineSeparator = '\n';
+
+            var arrayOfNumbers = numbers.Split(comaSeparator, newLineSeparator);
+
+            return arrayOfNumbers.Sum(Convert.ToInt32);
+        }
+    }
+}
